23f75c41d5722145ea854def96f06f28710853de
Claudius Kienle <claudius.kienle@tu-darmstadt.de>
2026-03-09 14:01:08 +0100

    refactor transition function to use frozenset for immutability

--- src/pddl_utils/utils/structs_functs.py
+++ src/pddl_utils/utils/structs_functs.py
@@ -15,13 +15,13 @@ from pddl_utils.structs.structs import (
 )
 
 
-def transition(curr_state: set[GroundAtom], effect: set[GroundAtom]) -> set[GroundAtom]:
+def transition(curr_state: frozenset[GroundAtom], effect: frozenset[GroundAtom]) -> frozenset[GroundAtom]:
     """Apply the effect to the current state and return the new state."""
     new_state = set(curr_state)
     for atom in effect:
         new_state.discard(GroundAtom(atom.predicate.get_negation(), atom.objects))  # remove negation if present
         new_state.add(atom)
-    return new_state
+    return frozenset(new_state)
 
 
 def get_substitutions(variables: Sequence[Variable], objects: frozenset[Object]) -> Generator[VarToObjSub, None, None]:
